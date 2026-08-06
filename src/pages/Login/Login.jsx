c136651f05547ef69f8e16dacdba62f1017601d1
Navya Kottakkeel <navyakottakkeel@gmail.com>
2026-08-06 22:03:17 +0530

    feat(auth): complete sign in and sign up UI

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,18 +1,22 @@
 import React from 'react'
 import './Login.css'
 import logo from '../../assets/logo.png'
+import { useState } from 'react'
 
 const Login = () => {
+
+  const [signState, setSignState] = useState('Sign In')
+
   return (
     <div className='login'>
       <img src={logo} alt="" />
       <div className="login-form">
-        <h1>Sign Up</h1>
+        <h1>{signState}</h1>
         <form action="">
-          <input type="text" placeholder='Your Name' />
+          {signState === 'Sign Up'? <input type="text" placeholder='Your Name' />:<></> }
           <input type="email" placeholder='Email' />
           <input type="password" placeholder='Password' />
-          <button>Sign Up</button>
+          <button>{signState}</button>
           <div className="form-help">
             <div className="remember">
               <input type="checkbox" />
@@ -21,6 +25,10 @@ const Login = () => {
             <p>Need Help?</p>
           </div>
         </form>
+        <div className="form-switch">
+          {signState === 'Sign In'? <p>New to Netflix? <span onClick={() => setSignState('Sign Up')}>Sign Up Now</span></p> :
+          <p>Already have Account? <span onClick={() => setSignState('Sign In')}>Sign In Now</span></p>}
+        </div>
       </div>
     </div>
   )
